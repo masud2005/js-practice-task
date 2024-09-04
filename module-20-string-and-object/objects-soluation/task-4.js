dcc08e738226223c9babbc2092e7d87a392fbcc6
masud2005 <masud20012005@gmail.com>
2024-09-05 02:34:22 +0600

    added module-20 object soluation folder but  not soluatio problem just added a folder and file

---
+++ module-20-string-and-object/objects-soluation/task-4.js
@@ -0,0 +1,18 @@
+
+/*
+Problem-4:
+    Count the number of properties.
+
+    Input:
+
+    let student = {
+        name: 'Ariana Grande',
+        age: 21,
+        city: 'Gaibandha',
+        isStudent: true
+    };
+    
+    Output:
+    4
+
+*/
---
+++ module-20-string-and-object/objects-soluation/task-5.js
@@ -0,0 +1,22 @@
+
+/*
+Problem-5:
+    Loop through an object and print the key-value pairs with their types
+
+    Input:
+
+    let myObject = {
+
+    name: 'John Doe',
+    age: 25,
+    city: 'Example City',
+    isStudent: true
+    };
+
+    Output:
+
+    key: name | type:  string
+    key: age | type:  number
+    key: city | type:  string
+    key: isStudent | type:  boolean
+*/
